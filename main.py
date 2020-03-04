ca860ce21480c6fda12af10638715b9d7e6ce6f8
joao pedro <joaopedro10782@gmail.com>
2020-03-04 14:24:26 -0300

    change message

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ port = int(os.environ.get("PORT", 5000))
 
 @app.route("/")
 def hello():
-    return "This is my site test"
+    return "I have dockerized my flask application and deployed it on heroku"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0",port=port)
