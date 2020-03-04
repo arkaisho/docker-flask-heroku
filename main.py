e03b8ddf3e9b14073580effd5eae2fa816d70a63
joao pedro <joaopedro10782@gmail.com>
2020-03-04 13:46:21 -0300

    a

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os
 app = Flask(__name__)
+
+port = int(os.environ.get("PORT", 5000))
 
 @app.route("/")
 def hello():
     return "This is my site test"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0",port=port)
